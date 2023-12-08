ed12335b81a643da139d40bf3d6fc5526075f114
Charbel Abi Daher <abidahercharbel@gmail.com>
2023-12-08 14:42:39 +0100

    Added play() and reset(). Added checks

--- core.py
+++ core.py
@@ -1,9 +1,12 @@
 import numpy as np
 
+
 class Table:
     def __init__(self, n=3):
-        self.size = n
-        self.board = np.zeros((n, n))
+        if n < 1:
+            raise ValueError("Size cannot be smaller than 1!")
+        self.size = int(n)
+        self.board = np.zeros((self.size, self.size))
         self.player = 1
 
     def detect_win(self):
@@ -19,9 +22,21 @@ class Table:
         # Diagonales
         if all(t[i][i] == t[0][0] for i in range(self.size)) and t[0][0]:
             return t[0][0]
-        if all(t[self.size-i-1][i] == t[self.size-1][0] for i in range(self.size)) and t[self.size-1][0]:
-            return t[self.size-1][0]
+        if (
+            all(t[self.size - i - 1][i] == t[self.size - 1][0] for i in range(self.size))
+            and t[self.size - 1][0]
+        ):
+            return t[self.size - 1][0]
         return 0
 
     def play(self, x, y):
-        pass
+        if x >= self.size or y >= self.size:
+            raise ValueError("Coordinates given are out of bounds.")
+        if self.board[x][y] != 0:
+            raise ValueError("Selected cell is not empty.")
+        self.board[x][y] = self.player
+        self.player *= -1
+
+    def reset(self):
+        self.board = np.zeros((self.size, self.size))
+        self.player = 1
