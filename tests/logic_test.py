ca04c4d53255cfb5bf926ebc9329df62425ab3c8
Charles Zablit <zablitcharles@gmail.com>
2023-12-08 14:57:19 +0100

    feat: add tests

--- tests/logic_test.py
+++
@@ -1,7 +0,0 @@
-import pathlib
-from unittest.mock import patch
-
-
-def test_is_in_julia():
-    """Test `is_in_julia`"""
-    assert True
---
+++ tests/test_core.py
@@ -0,0 +1,8 @@
+from tictactoe.core import Table
+
+
+def test_table_init():
+    """Test Table initialization"""
+    Table(1)
+    Table(2)
+    Table(3)
