f0a64f95fde72f040b1544e16eaecb44872ee479
Charbel Abi Daher <abidahercharbel@gmail.com>
2023-12-08 15:09:25 +0100

    Added extra tests

--- tests/test_core.py
+++ tests/test_core.py
@@ -17,6 +17,27 @@ def test_table_init_failing():
     Table(1)
 
 
+def test_play_out_of_bounds():
+    table = Table(3)
+    with pytest.raises(ValueError):
+        # Testing an out-of-bounds move
+        table.play(3, 0)
+
+
+def test_play_in_occupied_cell():
+    table = Table(3)
+    table.play(0, 0)
+    with pytest.raises(ValueError):
+        table.play(0, 0)
+
+
+def test_play_negative_index():
+    table = Table(3)
+    with pytest.raises(ValueError):
+        # Negative indices are not allowed
+        table.play(-1, 0)
+
+
 def test_table_win():
     """Test Table.win()"""
     table = Table(3)
